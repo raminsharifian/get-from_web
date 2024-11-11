5ed2224426929a42af715b37ce6252361791644d
Ramin Sharifian <136286510+raminsharifian@users.noreply.github.com>
2024-11-11 10:11:48 +0330

    add hrefs fetcher struct

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 from access.os_access import os_access
+from sys import argv
+
+URL = "http://localhost:3000"
 
 
 def main():
+    os.system(f"mkdir -p ./{argv[1]}")
+    os.system(f"curl -s {URL} | grep -oP '(?<=href=\")[^\"]*' > ./{argv[1]}/urls.txt")
+
     print("os: {}".format(os.name))
 
 
